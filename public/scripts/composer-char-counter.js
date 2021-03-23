a45470c82366f0f5e8e0fdbb323f6bd2e87319c8
Tessa Azar MacLean <tessaamac@gmail.com>
2021-03-23 23:20:13 +0000

    Add basic tweet layout. No hover compatibility yet.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
 // traversing DOM instead of using "$('.new-tweet .counter')"
 
 const countTweet = function() {
-  console.log(this);
   $(this).siblings().children('.counter').text(140 - $(this).val().length);
   if ($(this).siblings().children('.counter').text() < 0) {
     $(this).siblings().children('.counter').addClass("negative");
